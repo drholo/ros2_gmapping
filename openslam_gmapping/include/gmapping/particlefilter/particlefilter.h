318b2f5384bc8ec33183bc8ab68b439c4f4ca5a4
Andrii Kudriashov <andrii@kudriashov.net>
2026-01-20 14:36:21 +0100

    Fix compatibility with Jazzy

--- openslam_gmapping/include/gmapping/particlefilter/particlefilter.h
+++ openslam_gmapping/include/gmapping/particlefilter/particlefilter.h
@@ -137,9 +137,10 @@ void rle(OutputIterator& out, const Iterator & begin, const Iterator & end){
 			count=1;
 		}
 	}
-	if (count>0)
+	if (count>0) {
 		*out=std::make_pair(current,count);
 		out++;
+	}
 }
 
 //BEGIN legacy
