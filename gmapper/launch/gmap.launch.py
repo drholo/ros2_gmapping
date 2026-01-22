aa0eedac4f34712734e3ae12cf2370674c8f20dd
Andrii Kudriashov <andrii@kudriashov.net>
2026-01-22 10:45:56 +0100

    move gmapper launch as var

--- gmapper/launch/gmap.launch.py
+++ gmapper/launch/gmap.launch.py
@@ -1,12 +1,19 @@
 from launch import LaunchDescription
 from launch.substitutions import EnvironmentVariable
 import launch.actions
-import launch_ros.actions
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='true')
+
+    gmapper_node = Node(
+        package='gmapper',
+        executable='gmap',
+        output='screen',
+        parameters=[{'use_sim_time':use_sim_time}]
+        )
+
     return LaunchDescription([
-        launch_ros.actions.Node(
-            package='gmapper', node_executable='gmap', output='screen', parameters=[{'use_sim_time':use_sim_time}]),
+        gmapper_node
     ])
